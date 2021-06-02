6a8dcee6ca7490f9f8684abd224dee1b890e4b26
AB-Azad <azad.bbc@gmail.com>
2021-06-03 00:15:09 +0600

    Added images Fixed images link

--- includes/recommended-plugins/recommendations.php
+++ includes/recommended-plugins/recommendations.php
@@ -10,7 +10,7 @@
  * @param string    $hook_suffix use it to load this library assets only to the recommedded plugins page. Not into the whol admin area.
  *
  */
-$recommendations = new HTRP_Recommended_Plugins( $text_domain = 'htpm', $parent_menu_slug = 'htpm-options', $submenu_label = '', $submenu_page_name = '', $priority = '', $hook_suffix = 'plugin-manager_page_htpm_extension' );
+$recommendations = new HTRP_Recommended_Plugins( $text_domain = 'htpm', $parent_menu_slug = 'htpm-options', $submenu_label = '', $submenu_page_name = '', $priority = '', $hook_suffix = '' );
 
 $recommendations->add_new_tab(array(
     'title' => esc_html__( 'Pro Version', 'htpm' ),
@@ -24,7 +24,7 @@ $recommendations->add_new_tab(array(
             'link'      => 'https://hasthemes.com/plugins/wp-plugin-manager-pro/',
             'author_link'=> 'https://hasthemes.com/',
             'description'=> esc_html__( 'WP Plugin Manager Pro is a specialized WordPress Plugin that helps you to deactivate unnecessary WordPress Plugins page wise and boosts the speed of your WordPress site to improve the overall site performance.', 'htpm' ),
-            'image' => WHOLS_ASSETS . '/images/extensions/wp-plugin-manager-pro.jpeg'
+            'image' => HTPM_ROOT_URL . '/assets/images/extensions/wp-plugin-manager-pro.jpeg'
         ),
     )
 ));
@@ -67,7 +67,7 @@ $recommendations->add_new_tab(array(
             'link'      => 'https://hasthemes.com/wp/justtables/',
             'author_link'=> 'https://hasthemes.com/',
             'description'=> esc_html__( 'JustTables is an incredible WordPress plugin that lets you showcase all your WooCommerce products in a sortable and filterable table view. It allows your customers to easily navigate through different attributes of the products and compare them on a single page. This plugin will be of great help if you are looking for an easy solution that increases the chances of landing a sale on your online store.', 'htpm' ),
-            'image' => WHOLS_ASSETS . '/images/extensions/just-tables-pro.png'
+            'image' => HTPM_ROOT_URL . '/assets/images/extensions/just-tables-pro.png'
         ),
 
         array(
@@ -77,7 +77,7 @@ $recommendations->add_new_tab(array(
             'link'      => 'https://hasthemes.com/plugins/wc-builder-woocoomerce-page-builder-for-wpbakery/',
             'author_link'=> 'https://hasthemes.com/',
             'description'=> esc_html__( 'WC Builder Pro allows you to build Shop, Product Details, Cart, Checkout, My Account and Thank You page without even touching a single line of code! This plugin has 34+ custom addons for WooCommerce to build the page using WPBakery page builder. ', 'wpbforwpbakery' ),
-            'image' => WPBFORWPBAKERY_ADDONS_PL_URL . '/assets/images/extensions/wc-builder-pro.png'
+            'image' => HTPM_ROOT_URL . '/assets/images/extensions/wc-builder-pro.png'
         ),
 
         array(
@@ -87,7 +87,7 @@ $recommendations->add_new_tab(array(
             'link'      => 'https://hasthemes.com/plugins/multi-currency-pro-for-woocommerce/',
             'author_link'=> 'https://hasthemes.com/',
             'description'=> esc_html__( 'Multi-Currency Pro for WooCommerce is a prominent currency switcher plugin for WooCommerce. This plugin allows your website or online store visitors to switch to their preferred currency or their country’s currency.', 'htpm' ),
-            'image' => WHOLS_ASSETS . '/images/extensions/multicurrencypro.png'
+            'image' => HTPM_ROOT_URL . '/assets/images/extensions/multicurrencypro.png'
         ),
 
         array(
@@ -97,7 +97,7 @@ $recommendations->add_new_tab(array(
             'link'      => 'https://hasthemes.com/plugins/email-candy-pro/',
             'author_link'=> 'https://hasthemes.com/',
             'description'=> esc_html__( 'Email Candy is an outstanding WordPress plugin that allows you to customize the default WooCommerce email templates and give a professional look to your WooCommerce emails. If you are tired of using the boring design of WooCommerce emails and want to create customized emails, then this plugin will come in handy.', 'htpm' ),
-            'image' => WHOLS_ASSETS . '/images/extensions/email-candy-pro.png'
+            'image' => HTPM_ROOT_URL . '/assets/images/extensions/email-candy-pro.png'
         ),
 
     )
