93dda93d281aef5f7e54404c6fff16688ffcd91b
madhusudan <94890621+madhusudan-ht@users.noreply.github.com>
2025-05-08 15:17:09 +0600

    un used codes removed

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,35 +3,14 @@
     <!-- Header -->
     <Header />
     <el-row :gutter="30" class="htpm-content">
-    <el-col :span="19">
-      <slot></slot>
-    </el-col>
-    <el-col :span="5">
-      <!-- Sidebar -->
-      <sidebar />
-    </el-col>
-  </el-row>
-
-    <!-- Changelog Drawer -->
-    <el-drawer
-      v-model="changelogDialog"
-      title="Plugin Updates"
-      direction="rtl"
-      size="400px"
-      custom-class="changelog-drawer" style="top: 32px !important; height: calc(100vh - 32px) !important;"
-    >
-      <div class="changelog-wrapper">
-        <div v-for="update in updates" :key="update.id" class="changelog-item">
-          <div class="update-header">
-            <h3>{{ update.plugin }}</h3>
-            <span class="version">v{{ update.version }}</span>
-          </div>
-          <div class="changelog-content">
-            <p>{{ update.changelog }}</p>
-          </div>
-        </div>
-      </div>
-    </el-drawer>
+      <el-col :span="19">
+        <slot></slot>
+      </el-col>
+      <el-col :span="5">
+        <!-- Sidebar -->
+        <sidebar />
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -39,8 +18,6 @@
 import { ref } from 'vue'
 import Header from '../components/Header.vue'
 import Sidebar from '../components/Sidebar.vue'
-
-const changelogDialog = ref(false)
 
 </script>
 
